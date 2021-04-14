a3d554f3d54aa6a89efcf64439cbf245231bb841
Osehi <osehiaseehilen@gmail.com>
2021-04-14 18:50:07 +0100

    di-package created

--- app/src/main/java/com/polish/androidbackgroundcomponents/EntryApplication.kt
+++ app/src/main/java/com/polish/androidbackgroundcomponents/EntryApplication.kt
@@ -1,6 +1,8 @@
 package com.polish.androidbackgroundcomponents
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class EntryApplication: Application() {
 }
---
+++ app/src/main/java/com/polish/androidbackgroundcomponents/di/NetworkModule.kt
@@ -0,0 +1,7 @@
+package com.polish.androidbackgroundcomponents.di
+
+import dagger.Module
+
+@Module
+class NetworkModule {
+}
